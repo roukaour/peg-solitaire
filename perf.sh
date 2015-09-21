514f996bf582767d64dcd2cd3eec9a19c523db58
Remy Oukaour <remy.oukaour@gmail.com>
2015-09-20 20:52:49 -0400

    Add perf.xlsx.

--- perf.sh
+++ perf.sh
@@ -3,17 +3,14 @@
 echo Testing performance > perf.txt
 
 for board in boards/g*.txt; do
-	if [[ $board == *gcentral.txt ]]; then
-		continue
-	fi
-	if [[ $board == *gdiamond.txt ]]; then
-		continue
-	fi
+	#if [[ $board == *gcentral.txt ]]; then
+	#	continue
+	#fi
 	echo $board >> perf.txt
-	python pegSolitaire.py --input $board --flag 1 >> perf.txt
-	#python pegSolitaire.py --input $board --flag 2 >> perf.txt
-	#python pegSolitaire.py --input $board --flag 3 >> perf.txt
-	#python pegSolitaire.py --input $board --flag 4 >> perf.txt
-	#python pegSolitaire.py --input $board --flag 5 >> perf.txt
-	#python pegSolitaire.py --input $board --flag 6 >> perf.txt
+	#python pegSolitaire.py --input $board --flag 1 >> perf.txt
+	python pegSolitaire.py --input $board --flag 2 >> perf.txt
+	python pegSolitaire.py --input $board --flag 3 >> perf.txt
+	python pegSolitaire.py --input $board --flag 4 >> perf.txt
+	python pegSolitaire.py --input $board --flag 5 >> perf.txt
+	python pegSolitaire.py --input $board --flag 6 >> perf.txt
 done
